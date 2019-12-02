db2c3af71e7ce5e3030340e5ed266796ce93ec33
Kelvin Pereira Biserra <42652204+Kelvin-Pereira@users.noreply.github.com>
2019-12-02 08:37:17 -0300

    UPDATE correção id

--- assets/kanban.js
+++ assets/kanban.js
@@ -266,7 +266,7 @@
         });
         headerBoard.innerHTML =
           // '<div class="kanban-title-boards">' + board.title + "</div>";   // add quanticades de card
-          '<div class="kanban-title-boards">' + board.title + "<span class='size' id='"+ board.title +"'>"+ board.item.length +"</span></div>";   // add quanticades de card
+          '<div class="kanban-title-boards">' + board.title + "<span class='size' id='"+ board.id +"'>"+ board.item.length +"</span></div>";   // add quanticades de card
           var contentBoard = document.createElement("main");
         contentBoard.classList.add("kanban-drag");
         if (board.bodyClass !== "" && board.bodyClass !== undefined)
